030ddf927c044ea634f70431e7d794b372776917
shrihanshu <mshrihanshu@gmail.com>
2026-02-11 15:26:50 +0530

    coming soon

--- components/hero.jsx
+++ components/hero.jsx
@@ -46,11 +46,9 @@ const HeroSection = () => {
               Get Started
             </Button>
           </Link>
-          <Link href="https://www.youtube.com/roadsidecoder">
-            <Button size="lg" variant="outline" className="px-8">
-              Watch Demo
-            </Button>
-          </Link>
+          <Button size="lg" variant="outline" className="px-8" disabled>
+            Coming Soon
+          </Button>
         </div>
         <div className="hero-image-wrapper mt-5 md:mt-0">
           <div ref={imageRef} className="hero-image">
